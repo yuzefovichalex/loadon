7a0d3c46dc086a4896d89cc1f3423d3fce19df9f
Yuzefovich Alex <36435369+yuzefovichalex@users.noreply.github.com>
2020-08-21 17:09:05 +0200

    Implemented scaling text and state transitions

--- app/src/main/java/com/alexyuzefovich/sample/MainActivity.java
+++ app/src/main/java/com/alexyuzefovich/sample/MainActivity.java
@@ -3,12 +3,31 @@ package com.alexyuzefovich.sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.alexyuzefovich.loadon.Loadon;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean b = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Loadon loadon = findViewById(R.id.loadon);
+        loadon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!b) {
+                    loadon.startLoading();
+                    b = true;
+                } else {
+                    loadon.stopLoading();
+                    b = false;
+                }
+            }
+        });
     }
+
 }
--- loadon/src/main/java/com/alexyuzefovich/loadon/Loadon.java
+++ loadon/src/main/java/com/alexyuzefovich/loadon/Loadon.java
@@ -1,4 +1,181 @@
 package com.alexyuzefovich.loadon;
 
-public class Loadon {
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class Loadon extends View {
+
+    enum State {
+        NORMAL,
+        COLLAPSING,
+        LOADING,
+        EXTENDING
+    }
+
+    private static final int BASE_OFFSET = 32;
+
+    private static final long SIZE_ANIMATION_DURATION = 5000L;
+
+    private static final float DEFAULT_TEXT_SIZE = 15f;
+    private static final int DEFAULT_TEXT_COLOR = Color.BLACK;
+
+    private TextPaint textPaint = new TextPaint();
+
+    private String text = "";
+    private float textSize = DEFAULT_TEXT_SIZE;
+    private int textColor = DEFAULT_TEXT_COLOR;
+
+    private StaticLayout staticLayout;
+
+    private State state = State.NORMAL;
+
+    @NonNull
+    private ValueAnimator sizeAnimator = new ValueAnimator();
+
+    private int x;
+
+    public Loadon(Context context) {
+        super(context, null);
+    }
+
+    public Loadon(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        if (attrs != null) {
+            TypedArray ta = context.getTheme().obtainStyledAttributes(
+                    attrs,
+                    R.styleable.Loadon,
+                    0, 0);
+            initTextDrawing(ta);
+            ta.recycle();
+        }
+        initSizeAnimator();
+    }
+
+    public Loadon(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+    public Loadon(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void initTextDrawing(@NonNull TypedArray ta) {
+        text = ta.getString(R.styleable.Loadon_text);
+        textSize = ta.getDimension(R.styleable.Loadon_textSize, DEFAULT_TEXT_SIZE);
+        textColor = ta.getColor(R.styleable.Loadon_textColor, DEFAULT_TEXT_COLOR);
+
+        textPaint.setTextSize(textSize);
+        textPaint.setColor(textColor);
+        textPaint.setAntiAlias(true);
+
+        final int textWidth = (int) textPaint.measureText(text);
+        staticLayout = new StaticLayout(
+                text,
+                textPaint,
+                textWidth,
+                Layout.Alignment.ALIGN_NORMAL,
+                1f, 0, false);
+    }
+
+    private void initSizeAnimator() {
+        sizeAnimator.addUpdateListener(animation -> {
+            x = (int) animation.getAnimatedValue();
+            requestLayout();
+            invalidate();
+        });
+        sizeAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                switch (state) {
+                    case COLLAPSING:
+                    case LOADING: {
+                        state = State.EXTENDING;
+                        break;
+                    }
+                    case EXTENDING:
+                    case NORMAL: {
+                        state = State.COLLAPSING;
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                switch (state) {
+                    case COLLAPSING: {
+                        state = State.LOADING;
+                        break;
+                    }
+                    case EXTENDING: {
+                        state = State.NORMAL;
+                        break;
+                    }
+                }
+            }
+        });
+        sizeAnimator.setDuration(SIZE_ANIMATION_DURATION);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = state == State.NORMAL ? staticLayout.getWidth() + BASE_OFFSET * 2 : x;
+        int height = staticLayout.getHeight() + BASE_OFFSET * 2;
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.save();
+
+        if (state != State.NORMAL) {
+            float m = ((float) x - getHeight()) / (staticLayout.getWidth() + 64 - getHeight());
+            int alpha = (int) (m * 255);
+            float textSize = m * this.textSize;
+            textPaint.setAlpha(alpha);
+            textPaint.setTextSize(textSize);
+        } else {
+            textPaint.setTextSize(textSize);
+            textPaint.setAlpha(255);
+        }
+        canvas.translate(32, 32);
+
+
+        staticLayout.draw(canvas);
+        canvas.restore();
+    }
+
+    public void startLoading() {
+        if (state == State.LOADING) {
+            return;
+        }
+        sizeAnimator.cancel();
+        sizeAnimator.setIntValues(getWidth(), getHeight());
+        sizeAnimator.start();
+    }
+
+    public void stopLoading() {
+        if (state == State.NORMAL) {
+            return;
+        }
+        sizeAnimator.cancel();
+        sizeAnimator.setIntValues(getWidth(), staticLayout.getWidth() + 64);
+        sizeAnimator.start();
+    }
+
 }
